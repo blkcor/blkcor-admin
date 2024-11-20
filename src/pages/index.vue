beb756ab8549d21114beee5d2bd780203144b588
blkcor <blkcor.dev@gmail.com>
2024-11-21 00:04:43 +0800

    init project

---
+++ src/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>home</div>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { registerSW } from 'virtual:pwa-register'
+
+onMounted(() => {
+  registerSW({
+    immediate: true,
+    onRegisteredSW(_url, registration) {
+      console.log('🚀 ~ onRegisteredSW ~ registration:', registration)
+      console.log('🚀 ~ onRegisteredSW ~ _url:', _url)
+      setInterval(() => {
+        registration && registration.update()
+      }, 3600000)
+    }
+  })
+})
+</script>
+
+<style scoped></style>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
